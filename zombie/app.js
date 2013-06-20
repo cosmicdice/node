f8e427cd745c061faab849beb9e271b3742737f5
Antoni Galmiche <cosmicdice@gmail.com>
2013-06-20 12:09:17 +0200

    Installation de socket.io

--- zombie/app.js
+++ zombie/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var socket = require('socket.io');
 var app = express();
+var io = socket.listen(app);
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+io.sockets.on('connection', function(client){
+  console.log('client connected...');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
-console.log(app);
+
+//app.get('/', function(req, res){
+//  res.send('Hello World');
+//});
+
+//app.listen(8080);
+//console.log('Listening on port 8080');
